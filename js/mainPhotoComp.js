ec17f7d8d5ff30f93d8c112f8b7a9a33a43f68df
Nikita Sahare <nikita.cmow@gmail.com>
2018-07-03 19:36:05 +0530

    initial Edit

---
+++ js/mainPhotoComp.js
@@ -0,0 +1,18 @@
+/* var slideIndex=0;
+carousel();
+
+function carousel(){
+  var i;
+  var x = document.getElementByClassName("mySlide");
+  for (i = 0; i < x.length; i++){
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if(slideIndex > x.length){
+    slideIndex++;
+  }
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel,2000); //change image after every 2 second
+
+}
+*/
